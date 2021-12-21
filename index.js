6b798c2a5b96dbee26ea4d03caebfc60512ee1e2
dparikesit <dsparikesit@gmail.com>
2021-12-21 22:50:39 +0700

    added postgres and knex connection

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+require('dotenv').config()
+
 const express = require("express");
 const path = require("path");
 const app = express();
+
+const knex = require("knex")({
+  client: "pg",
+  version: "14.1",
+  connection: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE,
+  },
+  debug: true,
+});
 
 app.use(express.static(path.join(__dirname, "frontend/build")));
 
@@ -8,6 +23,8 @@ app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 3001;
 
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`Running on PORT ${PORT}`);
+});
