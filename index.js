ef2f45b298277e1f495405337f7af21f8e152493
dparikesit <dsparikesit@gmail.com>
2021-12-25 16:24:39 +0700

    Add knex migration system

--- index.js
+++ index.js
@@ -3,19 +3,7 @@ require('dotenv').config()
 const express = require("express");
 const path = require("path");
 const app = express();
-
-const knex = require("knex")({
-  client: "pg",
-  version: "14.1",
-  connection: {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_DATABASE,
-  },
-  debug: true,
-});
+// const db = require('./database/db')
 
 app.use(express.static(path.join(__dirname, "frontend/build")));
 
