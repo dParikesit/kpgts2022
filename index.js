8f7467630947a0826f708ec0c78e145e2fe4129a
dparikesit <dsparikesit@gmail.com>
2021-12-25 18:28:17 +0700

    Better routing

--- index.js
+++ index.js
@@ -6,14 +6,11 @@ const app = express();
 const routes = require('./routes/index')
 
 app.use(express.static(path.join(__dirname, "frontend/build")));
-
-app.use("/user", routes.user)
-app.use("/registration", routes.registration)
-app.use("/post", routes.post)
-
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
+
+app.use("/api", routes)
 
 const PORT = process.env.PORT || 3001;
 
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
+const router = require('express').Router()
+
 const user = require('./user')
 const registration = require('./registration')
 const post = require('./post')
 
+router.use("/user", user)
+router.use("/registration", registration)
+router.use("/post", post)
 
-module.exports = {user, registration, post}
+module.exports = router
