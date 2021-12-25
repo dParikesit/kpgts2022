672936611d544aae802ea3b6a58a485a9600ecf4
dparikesit <dsparikesit@gmail.com>
2021-12-25 20:13:51 +0700

    Add registration validation Moved misplaced frontend depencencies

--- routes/registration.js
+++ routes/registration.js
@@ -1,13 +1,18 @@
-const db = require('../database/db')
-const router = require('express').Router()
+const db = require('../database/db');
+const router = require('express').Router();
+const validator = require('validator');
 
 // Create TO Participant
-// TODO validate input
 router.post("/", async (req,res)=>{
     try{
-        await db('registration')
-            .insert(req.body)
-        res.status(200).json('Yey berhasil')
+        const valid = validator.isAlpha(req.body.nama.replace(/\s+/g, '')) && validator.isAlphanumeric(req.body.sekolah.replace(/\s+/g, '')) && (validator.equals(req.body.jurusan, 'IPA') || validator.equals(req.body.jurusan, 'IPS')) && validator.isAlphanumeric(req.body.kontak.replace(/\s+/g, '')) && validator.isUUID(req.body.user_id)
+        if(valid){
+            await db('registration')
+                .insert(req.body)
+            res.status(200).json('Yey berhasil')
+        } else{
+            throw "Validation error"
+        }
     }catch (e) {
         res.status(500).json({error: e})
     }
