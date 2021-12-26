9cea0c1e88a9cb7ad5476ae43e07cfe0c4748b78
Adityapnn811 <72344203+Adityapnn811@users.noreply.github.com>
2021-12-26 23:30:09 +0700

    Updated navbar

--- frontend/src/component/komponen_umum/Navbar.jsx
+++ frontend/src/component/komponen_umum/Navbar.jsx
@@ -144,8 +144,6 @@ const Navbar = () => {
                     </Link>
                   </div>
                 </div> 
-
-                <DrawerComponent />
             </Toolbar>
           </AppBar>
         )}
