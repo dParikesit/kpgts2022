9da27639f1905884c25b4667b4f4690c4e897951
Zeus-s <kristabdixx@gmail.com>
2021-12-27 13:06:20 +0700

    add post berita and image, not tested

--- routes/post.js
+++ routes/post.js
@@ -1,7 +1,9 @@
 const db = require('../database/db')['db'];
+const multer = require('multer');
 const {body, validationResult} = require('express-validator');
 const router = require('express').Router();
 const postController = require('../controller/post');
+
 
 // Berarti api ada di /api/post/
 // router.get("/", (req,res)=>{
@@ -26,7 +28,17 @@ router.get("/:id", async (req,res) => {
     }
 });
 
-router.post("/add", async (req, res) => {
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, `./${process.env.IMAGE_FOLDER}`);
+    },
+    filename: (req,file,cb) =>{
+        cb(null, `post-${path.extname(file.originalname)}`);
+    }
+});
+const upload = multer({storage: fileStorage});
+
+router.post("/add", upload.single('image'),  async (req, res) => {
     body('title','Please fill in the title.').notEmpty();
     body('content','Please fill in the content.').notEmpty();
     const errors = validationResult(req); // Finds the validation errors in this request and wraps them in an object with handy functions
@@ -37,7 +49,17 @@ router.post("/add", async (req, res) => {
     const post = await postController.getOne('title', req.body.title);
     if (!(post.title.length > 0)) {
         try {
-            await postController.insert(req.body);
+            let data = {
+                title: req.body.title,
+                content: req.body.content,
+                picture:'',
+                user_id: req.session.sid // FIX INI
+            }
+            if(req.file)
+                picture = `post-${path.extname(req.file.originalname)}`;
+            data.picture = picture;
+
+            await postController.insert(data);
         } catch(e) {
             res.status(500).json({error: e});
         }
