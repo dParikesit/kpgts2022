5d6298f12bf24b5f15d30e0834d1287e728470ac
paramadwityatmaja <pdwitya@gmail.com>
2022-01-02 09:51:38 +0700

    push

--- frontend/src/component/homepage/Homepage.jsx
+++ frontend/src/component/homepage/Homepage.jsx
@@ -27,7 +27,21 @@ const Homepage = () => {
             <NavbarLoggedIn></NavbarLoggedIn>
             {/* <Navbar /> */}
             {/* <Peserta></Peserta> */}
-            <p>Homepage</p>
+            <div class="tulisan">
+                <div class="judul">
+                    <h1>KPGTS 2022</h1>
+                </div>
+                <div class="content">
+                    Selamat datang di KPGTS 2022. KPGTS adalah Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel dolore excepturi exercitationem illo molestias unde tenetur, delectus consectetur est mollitia rem ad dolorem commodi minus nisi neque porro. Vero, quae.
+                </div>
+            </div>
+            <div class="image">
+                <img src="" alt="Foto lambang KPGTS 2022">
+            </div>
+            <div class="button">
+                <button>Register</button>
+                <button>Timeline</button>
+            </div>
         </div>
     )
 }
