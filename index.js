0200fa8795cd4064b66526e8502f67a85482e033
dparikesit <dsparikesit@gmail.com>
2022-01-10 11:25:10 +0700

    Fix production build path

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use("/api", routes);
 
 app.use(express.static(path.join(__dirname, "frontend/build")));
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
 });
 
 
