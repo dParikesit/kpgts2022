1f3ccb95c1fdd8f9200bc80aca9e5fc875cb2ee4
dparikesit <dsparikesit@gmail.com>
2022-01-10 12:34:11 +0700

    Fix cookie pt1

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ app.use( session({
     httpOnly: true,
     // secure: !(environment==='development'),
     secure: true,
+    sameSite: "strict",
     maxAge: 1000 * 60 * 60 * 24 // Time is in miliseconds
 },
   resave: false
