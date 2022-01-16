aa621154328b134fff4f27f873d092f2f225e8bb
dparikesit <dsparikesit@gmail.com>
2022-01-16 18:30:34 +0700

    Fix mailer

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const nodemailer = require("nodemailer");
 const Email = require('email-templates');
 
@@ -14,8 +15,8 @@ const transporter = nodemailer.createTransport({
 const email = new Email({
     views: { root:'./templates', options: { extension: 'ejs' } },
     message: {
-        // from: process.env.MAILER_FROM
-        from: "noreply@kpgts2022.com"
+        from: process.env.MAILER_FROM
+        // from: "noreply@kpgts2022.com"
     },
     preview: process.env.NODE_ENV === 'development',
     send: !(process.env.NODE_ENV === 'development'),
